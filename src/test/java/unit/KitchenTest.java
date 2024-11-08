1dc3145887bf4532718218253c0a1950fbaee7b6
Ivan <yushkov_i.o@mail.ru>
2024-11-08 07:06:18 +0500

    new

---
+++ src/test/java/unit/KitchenTest.java
@@ -0,0 +1,37 @@
+package unit;
+
+import com.javaacademy.burger.*;
+import com.javaacademy.burger.exception.KitchenHasNoGasException;
+import com.javaacademy.burger.exception.UnsupportedDishTypeException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.javaacademy.burger.dish.DishType.BURGER;
+import static com.javaacademy.burger.dish.DishType.FUAGRA;
+
+public class KitchenTest {
+
+    @Test
+    @DisplayName("Успешное приготовление бургера")
+    public void cookBurger() {
+        Kitchen kitchen = new Kitchen();
+        kitchen.cook(BURGER);
+        Assertions.assertNotNull(kitchen.getCompletedDishes());
+    }
+
+    @Test
+    @DisplayName("Бургер с ошибкой")
+    public void cookBurgerFailure() {
+        Kitchen kitchen = new Kitchen();
+        kitchen.setHasGas(false);
+        Assertions.assertThrows(KitchenHasNoGasException.class, () -> kitchen.cook(BURGER));
+    }
+
+    @Test
+    @DisplayName("Фуагра с ошибкой")
+    public void cookFuagraFailure() {
+        Kitchen kitchen = new Kitchen();
+        Assertions.assertThrows(UnsupportedDishTypeException.class, () -> kitchen.cook(FUAGRA));
+    }
+}
